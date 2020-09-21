702783e59dcfb5fc816ad87d0351fad76417058e
Joshua James Lewis <josh1205_1933@yahoo.com>
2020-09-21 14:12:20 +0200

    Add project files.

---
+++ ConsoleConsumer/AreaRectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleConsumer
+{
+    public class AreaRectangle
+    {
+        public class AreaRec
+        {
+            public int Length { get; set; }
+            public int Width { get; set; }
+
+            /// <summary>
+            /// Empty constructor
+            /// </summary>
+            public AreaRec()
+            {
+
+            }
+
+            /// <summary>
+            /// Parameterized Constructor
+            /// </summary>
+            public AreaRec(int length, int width)
+            {
+                Length = length;
+                Width = width;
+            }
+
+            // append with comma because when the length and width properties are sent, they are sent with a comma
+            //check fiddler {"Length":2, "Width":3} i guess this is the json format
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(Length);
+                sb.Append(",");
+                sb.Append(Width);
+                return sb.ToString();
+            }
+        }
+    }
+}
---
+++ ConsoleConsumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert Length here");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insert Width here");
+            int width = int.Parse(Console.ReadLine());
+
+            AreaRectangle.AreaRec rect = new AreaRectangle.AreaRec(length,width);
+            var result = AreaRecAsync(rect).Result;
+            Console.WriteLine("Final result is " + result);
+            Console.ReadKey();
+        }
+
+        public static async Task<string> AreaRecAsync(AreaRectangle.AreaRec rec)
+        {
+            string result = null;
+            using (HttpClient client = new HttpClient())
+            {
+                var jsonstring = JsonConvert.SerializeObject(rec);
+                StringContent content = new StringContent(jsonstring, Encoding.UTF8,"application/json");
+
+                HttpResponseMessage response =
+                    await client.PostAsync("https://localhost:44372/Rectangle/AreaRectangle", content);
+                if (response.StatusCode != HttpStatusCode.Conflict)
+                {
+                    response.EnsureSuccessStatusCode();
+                    result = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    throw new Exception("Service is not running");
+                }
+            }
+
+            return result;
+        }
+    }
+}
